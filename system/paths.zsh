81902d72f29549f54f9841bec80f078fd9c50908
Arnold Almeida <arnold.almeida@gmail.com>
2013-02-04 11:46:25 +1100

    Added support for NPM

--- system/paths.zsh
+++ system/paths.zsh
@@ -2,7 +2,8 @@
 LOCAL="~/bin/"
 HOMEBREW="/usr/local/sbin"
 NODE_PATH="/usr/local/lib/node"
+NPM_PATH="/usr/local/share/npm/bin"
 HUBOT="node_modules/.bin"
 PHP="/usr/local/Cellar/php/5.3.10/bin"
 
-export PATH=$HUBOT:$NODE_PATH:$PHP:$HOMEBREW:$CUSTOM:$PATH
+export PATH=$HUBOT:$NODE_PATH:$NPM_PATH:$PHP:$HOMEBREW:$CUSTOM:$PATH
