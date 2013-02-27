56373b34dfc20426de9f6466aa1cec1b5fed3afa
Arnold Almeida <arnold.almeida@gmail.com>
2013-02-27 23:18:03 +1100

    Added support alias for composer in cakephp

--- cakephp/aliases.zsh
+++ cakephp/aliases.zsh
@@ -1,3 +1,8 @@
 # Alias functions
 alias cake='app/Console/cake'
 alias cakelogs='tail -f app/tmp/logs/*'
+
+# Composer
+alias compsetup='wget https://github.com/uzyn/cakephp-composer/tarball/master'
+alias comprequire='app/Console/cake composer.c require'
+alias compinstall='app/Console/cake composer.c install'
