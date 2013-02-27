ada6313ee880ff8ccc9e1adc5af68aa20677adaf
Arnold Almeida <arnold.almeida@gmail.com>
2013-02-27 23:18:45 +1100

    new git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gtoday="git log --since '1 day ago' --oneline --author Arnold"
+alias gammend="git commit --amend -C HEAD"
