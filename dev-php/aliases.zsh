ef315ff3800873ae5a3bce4b5967a396a6b138d2
Arnold Almeida <arnold.almeida@gmail.com>
2013-10-15 18:16:10 +1100

    Added generic php aliases

--- dev-php/aliases.zsh
+++ dev-php/aliases.zsh
@@ -1,8 +1,16 @@
-# Alias functions
+# Framwork agnostic
+alias composer='composer.phar'
+
+# Laravel
+alias art='artisan'
+
+
+# CakePhp core
 alias cake='app/Console/cake'
 alias cakelogs='tail -f app/tmp/logs/*'
 
-# Composer
+# Cakephp composer shiz
 alias compsetup='wget https://github.com/uzyn/cakephp-composer/tarball/master'
 alias comprequire='app/Console/cake composer.c require'
 alias compinstall='app/Console/cake composer.c install'
+
