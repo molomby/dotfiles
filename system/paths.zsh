75846a9e03ab760b5e81d1dcd735f9cfaa3d888a
Arnold Almeida <arnold.almeida@gmail.com>
2015-01-05 12:48:46 +1100

    Misc updates

--- system/paths.zsh
+++ system/paths.zsh
@@ -6,4 +6,4 @@ NPM_PATH="/usr/local/share/npm/bin"
 HUBOT="node_modules/.bin"
 PHP="/usr/local/Cellar/php/5.3.10/bin"
 
-export PATH=$HUBOT:$NODE_PATH:$NPM_PATH:$PHP:$HOMEBREW:$CUSTOM:$PATH
+export PATH=$HUBOT:$NODE_PATH:$NPM_PATH:$PHP:$HOMEBREW:$PATH
