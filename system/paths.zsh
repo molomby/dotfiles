f0fd4d9927587aae46226f043d2526e57c11ac09
Arnold Almeida <arnold@floatingpoints.com.au>
2015-03-05 16:53:27 +1100

    $PATH cleanup

--- system/paths.zsh
+++ system/paths.zsh
@@ -1,9 +1,9 @@
 # Paths
-LOCAL="~/bin/"
+DOTFILES="~/.dotfiles/bin"
 HOMEBREW="/usr/local/sbin"
 NODE_PATH="/usr/local/lib/node"
 NPM_PATH="/usr/local/share/npm/bin"
 HUBOT="node_modules/.bin"
 PHP="/usr/local/Cellar/php/5.3.10/bin"
 
-export PATH=$HUBOT:$NODE_PATH:$NPM_PATH:$PHP:$HOMEBREW:$PATH
+export PATH=$DOTFILES:$HUBOT:$NODE_PATH:$NPM_PATH:$PHP:$HOMEBREW:$PATH
